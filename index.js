150262f5f8718136630e19f1d33e916e323db346
vetrikumar4 <vetrikumar1998@gmail.com>
2025-02-06 09:30:03 +0530

    commit 5

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-// Import packages
-const express = require("express");
-const home = require("./fetch_data_from_ocr2");
-
-// Middlewares
-const app = express();
-app.use(express.json());
-app.get("/check", (req, res) => {
-  res.send("working");
-});
-// Routes
-app.use("/home", home);
-
-// connection
-const port = 3040;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+[
+  {
+    "ID Number": {
+      extracted_content: 784198136385261,
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    Name: {
+      extracted_content: "Milad William Tawfik Hanna",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Date of Birth": {
+      extracted_content: "1981-01-07",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    Nationality: {
+      extracted_content: "Egypt",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Issuing Date": {
+      extracted_content: "2024-01-02",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Expiry Date": {
+      extracted_content: "2025-12-31",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    Sex: {
+      extracted_content: "M",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Card Number": {
+      extracted_content: "134014085",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    Occupation: {
+      extracted_content: "Debts Collector",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Employer or Sponsor": {
+      extracted_content: "Rakez / Alnuaimi Group Fz-Llc",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+    "Issuing Place": {
+      extracted_content: "Ras Al Khaimah",
+      confidence_score: 100,
+      confidence_metric: "Very Confident",
+    },
+  },
+];
